74a9507798d49b792a5bf9a032cb1c0c8eaa6fdc
Hannah Malm <hannahemalm616@gmail.com>
2021-08-23 20:28:08 -0400

    commit tree 367aca0817f97633a1f4f8f87c9a7ea64743f85d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,44 +2,80 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
 
+
+  #the act of logging in is the action of storing a users ID in the session hash
+
   configure do
     enable :sessions
     set :session_secret, "secret"
   end
 
   get '/' do
-    # your code here
+    # render the app/views/home.erb
+    erb :home
   end
+  #1. User visits login page and fills in form with email and pass. Hit submit to POST data to controller route
+  #Get new users name, email and password from params hash
+  #Use info to create and save a new instance of User - User.create(name: params[:name], email: params[:email], password: params[:password])
+  #Once the user signs up, they are logged in by setting the session - session[:user_id]
+  #redirect to the hompage
+
+
 
   get '/registrations/signup' do
-    # your code here
+    # render the sign up form view
+    #app/views/registrations/signup.erb
+    erb :'/registrations/signup'
   end
+ #2. Controller route accesses the user email and pass from params hash
+ #info is used to find the user from the data base using User.find_by(emai: params[:email], password: params[:password])
+ #This info is stored as the value of session[:user_id]
+
 
   post '/registrations' do
-    # your code here
-
+    # responsible for handing POST request sent when a user hits Submit on sign up form
+    #contains code that gets the new user's info from the params hash - creates a new user, signs them in, redirects them
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save 
+    #save the session
+    session[:user_id] = @user.id
     redirect '/users/home'
   end
 
-  get '/users/home' do
-    # your code here
-  end
 
+  #introspect on the session hash in any other controller route and grab the current user by matching a user ID
+  #session[:user_id]
   get '/sessions/login' do
     # the line of code below render the view page in app/views/sessions/login.erb
+    #Render the login form
     erb :'sessions/login'
   end
 
-  post '/sessions' do
-    # your code here
 
-    redirect '/users/home'
+  post '/sessions' do
+    # Receive the POST request that gets sent when a user hits "submit" on login form
+    #route contains code that grabs the user's info from the params hash
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user 
+      session[:user_id] = @user.id 
+      redirect '/users/home'
+    end 
+    redirect '/sessions/login'
   end
 
+  #logging out means terminating the session - clears all the data from the session hash - use Ruby method #clear
   get '/sessions/logout' do
-    # your code here
-    
+    # responsible for logging the user out and clearning the hash
+    session.clear
     redirect '/'
   end
 
+  get '/users/home' do
+    # renders the users homepage view 
+    #in the view page use the <%= => tags to show the user and then do @user.name
+    @user = User.find(session[:user_id])
+    erb :'/users/home'
+  end
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 end
+
+#user model has a few attributes - name, email, password
+#this validates if a name is invalid or an email or password
+#THIS IS THE FIRST STEP 
+#run rake db:create_migration to write a migration that creates a Users table with columns for name, email, pass
+#run rake db:migrate SINATRA_ENV=test
